cf421164c0f782c1860ef11d780264f1a6885feb
Saquib472 <saquibali472@gmail.com>
2025-12-01 23:10:10 +0530

    Initial Commit of Lets Get Classy

---
+++ 08_Lets_Get_Classy/Code/src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import UserFuncComp from "./UserFuncComp";
+
+const AboutUs = ()=>{
+    return (
+        <div>
+            <h1>This is About Us</h1>
+            <UserFuncComp/>
+        </div>
+    )
+}
+
+export default AboutUs;
---
+++ 08_Lets_Get_Classy/Code/src/components/UserFuncComp.js
@@ -0,0 +1,10 @@
+const UserFuncComp = () => {
+    return (
+        <div>
+            <h1>Najmush Saquib Ali</h1>
+            <h3>Burdwan</h3>
+        </div>
+    )
+}
+
+export default UserFuncComp
